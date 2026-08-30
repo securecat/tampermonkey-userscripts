e999880e36dc9ff00afb3243a472babdbc0cec11
Yu Morita <securecat@gmail.com>
2026-08-30 22:15:21 +0900

    Update namespace URL in user script

--- tokyo-futari-story-newtab.user.js
+++ tokyo-futari-story-newtab.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TOKYOふたりSTORY - プロフィールを新しいタブで開く
-// @namespace    tokyo-futari-story-newtab
+// @namespace    https://github.com/securecat/tampermonkey-userscripts
 // @version      1.1
 // @description  プロフィール詳細を新しいタブで開く
 // @author       https://github.com/securecat
