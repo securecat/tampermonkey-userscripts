b541654c1af275ee9c85470e9dcb6b102b8fc3c1
Yu Morita <securecat@gmail.com>
2026-08-30 19:54:13 +0900

    Add user script to hide sub-navigation menu
    This user script hides the sub-navigation menu on the Takarakuji official site.

---
+++ takarakuji-official-hide-subnav.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         宝くじ公式サイト - 下層メニュー非表示
+// @namespace    http://tampermonkey.net
+// @version      1.0
+// @description  宝くじ公式サイトのグローバルナビのホバーで出る下層メニューを非表示にします
+// @author       https://github.com/securecat
+// @match        https://www.takarakuji-official.jp/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`
+        .l_globalNav_sub {
+            display: none !important;
+        }
+    `);
+})();
