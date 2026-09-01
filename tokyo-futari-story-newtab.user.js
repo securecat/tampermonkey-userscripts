525b0b56b00c31a25d4e90edf1522a6d36fddae5
Yu Morita <securecat@gmail.com>
2026-09-01 12:18:16 +0900

    Update user script to open profiles and photos in new tabs

--- tokyo-futari-story-newtab.user.js
+++ tokyo-futari-story-newtab.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         TOKYOふたりSTORY - プロフィールを新しいタブで開く
-// @namespace    https://github.com/securecat/tampermonkey-userscripts
-// @version      1.1
-// @description  プロフィール詳細を新しいタブで開く
+// @name         TOKYOふたりSTORY - プロフィールと写真を新しいタブで開く
+// @namespace    tokyo-futari-story-newtab
+// @version      1.2
+// @description  プロフィールと写真を新しいタブで開く
 // @author       https://github.com/securecat
 // @match        https://mypage-tokyo.parms-net.jp/mypr/list*
 // @match        https://mypage-tokyo.parms-net.jp/mypr/favorite
+// @match        https://mypage-tokyo.parms-net.jp/mypr/detail/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -24,10 +25,35 @@
         return originalSubmit.call(this);
     };
 
-    // お気に入り一覧：プロフィールへのリンクを新しいタブで開く
     document.addEventListener('DOMContentLoaded', () => {
+
+        // お気に入り一覧：プロフィールへのリンクを新しいタブで開く
         document.querySelectorAll('a[href*="/mypr/detail/"]').forEach(link => {
             link.target = '_blank';
         });
+
+        // バストアップ写真：新しいタブで開く
+        document.querySelectorAll('img.profile_photo[alt="バストアップ写真"]').forEach(img => {
+            const link = document.createElement('a');
+
+            link.href = img.src;
+            link.target = '_blank';
+
+            img.replaceWith(link);
+            link.appendChild(img);
+        });
+
+        // プライベート写真：ライトボックスを無効にして新しいタブで開く
+        document.querySelectorAll('a.boxer img.profile_photo[alt="プライベート写真"]').forEach(img => {
+            const oldLink = img.closest('a');
+            const newLink = oldLink.cloneNode(true);
+
+            newLink.classList.remove('boxer');
+            newLink.target = '_blank';
+
+            oldLink.replaceWith(newLink);
+        });
+
     });
+
 })();
