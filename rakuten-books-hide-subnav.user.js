825cd176f8b58bc9f8d652f013287a880b07da0b
Yu Morita <securecat@gmail.com>
2026-09-14 03:04:02 +0900

    Replace GM_addStyle with MutationObserver for submenu hiding
    Refactor to use MutationObserver for hiding submenus dynamically.

--- rakuten-books-hide-subnav.user.js
+++ rakuten-books-hide-subnav.user.js
@@ -5,16 +5,30 @@
 // @description  楽天ブックスのグローバルナビのホバーで表示される下層メニューを非表示にします
 // @author       https://github.com/securecat
 // @match        https://books.rakuten.co.jp/search*
-// @grant        GM_addStyle
+// @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    GM_addStyle(`
-        .sub-genre-popup {
-            display: none !important;
-        }
-    `);
+    const hideSubMenus = () => {
+        document.querySelectorAll('#js-subGenre .sub-genre-popup').forEach(el => {
+            el.style.setProperty('display', 'none', 'important');
+        });
+    };
+
+    // DOMが構築された後にも実行
+    const observer = new MutationObserver(() => {
+        hideSubMenus();
+    });
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ['class', 'style']
+    });
+
+    hideSubMenus();
 })();
